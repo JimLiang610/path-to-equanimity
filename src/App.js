5ad0e183c8b150eb48c168180b4fd2eeb06f5d36
Jim <zhijianliang09@gmail.com>
2024-11-04 20:28:06 -0800

    initial Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './homepage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+        <Home></Home>
     </div>
   );
 }
